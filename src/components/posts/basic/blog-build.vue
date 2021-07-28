d1ad03de24fb823717008d486a7cf1cd39c710bf
Liusq-Cindy <306749392@qq.com>
2021-07-28 22:40:32 +0800

    feat: 路由改动

---
+++ src/components/posts/basic/blog-build.vue
@@ -0,0 +1,18 @@
+<!-- 博客管理首页 -->
+<template>
+  <div class="arti">
+   <blog-artical></blog-artical>
+  </div>
+</template>
+
+<script>
+
+import BlogArtical from './blog-build.md'
+
+export default {
+  name: 'blog-build',
+  components: {
+    BlogArtical
+  }
+}
+</script>
---
+++ src/components/posts/basic/shuati.vue
@@ -0,0 +1,18 @@
+<!-- 博客管理首页 -->
+<template>
+  <div class="arti">
+   <blog-artical></blog-artical>
+  </div>
+</template>
+
+<script>
+
+import BlogArtical from './shuati.md'
+
+export default {
+  name: 'shuati',
+  components: {
+    BlogArtical
+  }
+}
+</script>
